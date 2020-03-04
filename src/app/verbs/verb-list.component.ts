e47ba725d65f28d36f1621d9e5d94e3a73b11cf7
brycehayes <bryce.hayes.scotland@gmail.com>
2020-03-04 16:39:01 +0000

    Added new verb feature

---
+++ src/app/verbs/verb-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+  selector: "verb-director",
+  templateUrl: "verb-list.component.html"
+})
+export class VerbListComponent {
+    pageTitle: string = "Verb Conjugation: Angular learning project";
+}
